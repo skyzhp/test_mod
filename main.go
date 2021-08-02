212746e10cabb160642fc6adba793ead90cccde8
zhaohaipeng <1039116456@qq.com>
2021-08-02 17:10:52 +0800

    test rm

--- main.go
+++ main.go
@@ -4,9 +4,34 @@ import (
 	"fmt"
 	"github.com/skyzhp/util"
 	a "github.com/skyzhp/util/a"
+	"time"
 )
 
 func main() {
 	fmt.Println(util.Add(1, 1))
-    a.LogTest("111")
+	a.LogTest("111")
+
+	arr := []int{}
+	arr = append(arr, 1)
+	fmt.Printf("%p\n", arr)
+	testArray(arr)
+	fmt.Println(arr)
+
+	arr2 := make([]int, 100)
+
+	for {
+		for i := 0; i < 100; i++ {
+			//index := i
+			go func() {
+				arr2[0] = 3
+			}()
+		}
+		time.Sleep(time.Second)
+	}
+
+}
+func testArray(arr []int) {
+	fmt.Println(arr)
+	fmt.Printf("%p\n", arr)
+	arr[0] = 2
 }
